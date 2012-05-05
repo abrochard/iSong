ce459cc5be9ed903603d8e97bb47f4441e587f42
Dina Lamdany <dinalamdany@gmail.com>
2012-05-05 15:45:18 -0400

    commented song

--- Song.java
+++ Song.java
@@ -54,74 +54,146 @@ public class Song {
 		this.retriever = retriever;
 	}
 
+	/**
+	 * Gets title
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * Sets title
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/**
+	 * Gets artist
+	 * @return artst
+	 */
 	public String getArtist() {
 		return artist;
 	}
 
+	/**
+	 * Sets artist
+	 * @param artist
+	 */
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
 
+	/**
+	 * Gets album
+	 * @return album
+	 */
 	public String getAlbum() {
 		return album;
 	}
 
+	/**
+	 * Sets album
+	 * @param album
+	 */
 	public void setAlbum(String album) {
 		this.album = album;
 	}
 
+	/**
+	 * Gets bitrate
+	 * @return bitrate
+	 */
 	public int getBitrate() {
 		return bitrate;
 	}
 
+	/**
+	 * Sets bitrate
+	 * @param bitrate
+	 */
 	public void setBitrate(int bitrate) {
 		this.bitrate = bitrate;
 	}
 
+	/**
+	 * Gets song duration
+	 * @return duration
+	 */
 	public int getDuration() {
 		return duration;
 	}
 
+	/**
+	 * Sets song duration
+	 * @param duration
+	 */
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
 
+	/**
+	 * Gets song size
+	 * @return size
+	 */
 	public double getSize() {
 		return size;
 	}
 
+	/**
+	 * Sets size of song
+	 * @param size
+	 */
 	public void setSize(double size) {
 		this.size = size;
 	}
 
+	/**
+	 * Gets beats per minute
+	 * @return BPM
+	 */
 	public int getBPM() {
 		return bpm;
 	}
 
+	/**
+	 * Sets beats per minute
+	 * @param bpm
+	 */
 	public void setBPM(int bpm) {
 		this.bpm = bpm;
 	}
 
+	/**
+	 * Gets song URL 
+	 * @return url
+	 */
 	public String getSource() {
 		return source;
 	}
 
+	/**
+	 * Sets song url
+	 * @param source
+	 */
 	public void setSource(String source) {
 		this.source = source;
 	}
 
+	/**
+	 * Gets retriever used for song
+	 * @return retriever
+	 */
 	public String getRetriever() {
 		return retriever;
 	}
 
+	/**
+	 * Sets retriever used for song
+	 * @param retriever
+	 */
 	public void setRetriever(String retriever) {
 		this.retriever = retriever;
 	}
